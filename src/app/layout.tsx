817240b50ca2b2b731a5d371c3751ca6684f95ea
bdeguio <77205811+bdeguio@users.noreply.github.com>
2025-04-08 03:10:29 +0000

    revise

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,7 @@
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { type Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,25 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
-          <header className="flex justify-between items-center px-6 py-4 border-b shadow-sm">
-            <Link href="/" className="text-xl font-bold">
-              Arena
-            </Link>
-
-            <div className="flex gap-4 items-center">
-              <SignedOut>
-                <SignInButton />
-                <SignUpButton />
-              </SignedOut>
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </div>
-          </header>
-
-          <main>{children}</main>
-        </body>
+        <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
   );
