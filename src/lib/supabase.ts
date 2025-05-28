c321e0a58769d610842ab87d7ad3000382280b74
bdeguio <77205811+bdeguio@users.noreply.github.com>
2025-05-28 19:13:09 +0000

    u4

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,14 +1,13 @@
-import { createClient } from '@supabase/supabase-js';
+// lib/supabase.ts
+import { createServerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
 
-export function createSupabaseClient(isServer: boolean = false) {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const supabaseKey = isServer
-    ? process.env.SUPABASE_SERVICE_ROLE_KEY!
-    : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-  if (!supabaseUrl || !supabaseKey) {
-    throw new Error('Supabase URL or Key is missing!');
-  }
-
-  return createClient(supabaseUrl, supabaseKey);
+export function createSupabaseClient() {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      cookies,
+    }
+  );
 }
